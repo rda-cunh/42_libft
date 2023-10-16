d23161fb97f2c3081e0bda695a60177a6b2c05e0
Ricardo Miguel Da Cunha Mendes <rda-cunh@c1r6s5.42porto.com>
2023-10-16 18:27:42 +0100

    new strlen and libft

--- ft_strlen.c
+++ ft_strlen.c
@@ -3,11 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 19:20:08 by rda-cunh          #+#    #+#             */
-/*   Updated: 2023/10/06 19:20:15 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2023/10/10 18:39:08 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/*
+#include <stdio.h>
+
+int main(void)
+{
+	char s[] = "teste2";
+	
+	printf("My function gives = %zu\n", ft_strlen(s));
+	printf("Original function gives = %zu\n", strlen(s));	
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -1,12 +1,36 @@
-/*include 42 header*/
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 17:54:20 by rda-cunh          #+#    #+#             */
+/*   Updated: 2023/10/12 20:05:59 by rda-cunh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef LIBFT_H
-#define my_header_file
+# define LIBFT_H
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+# include <string.h>
+//don't forget to verify at the end that all the needed libraries are here//
+
+//LIBC FUNCTIONS//
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strle(const char *s);
+
+//ADITIONAL FUNCTIONS//
+
+//BONUS FUNCTIONS//
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
 #endif
