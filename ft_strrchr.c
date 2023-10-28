99f6e1ba892a43cfc37820424c4507b34ba6eaed
rda-cunh <ricard.mendes@gmail.com>
2023-10-29 00:33:38 +0100

    updated funcrions

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rda-cunh <rda-cunh@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rmendes <rmendes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 18:39:36 by rda-cunh          #+#    #+#             */
-/*   Updated: 2023/10/24 19:06:06 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2023/10/29 00:31:31 by rmendes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	char	altc;
 	char	*alts;
@@ -38,7 +38,7 @@ int	main(void)
 	char str[] = "Vai ser um teste";
 	char c = 't';
 
-	printf("My function: %s\n", ft_strchr(str, c));
-	printf("My function: %s\n", strchr(str, c));
+	printf("My function: %s\n", ft_strrchr(str, c));
+	printf("My function: %s\n", strrchr(str, c));
 }
 */
