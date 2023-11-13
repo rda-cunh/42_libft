fdd0b3eb7cba58d8dab58bd97999deff30e0aff7
Ricardo Mendes <147749667+rda-cunh@users.noreply.github.com>
2023-11-13 18:18:28 +0000

    new

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -14,5 +14,10 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
+	t_list  *tmp
+
+    if (*lst == NULL || new == NULL)
+        return ;
+    new -> next = *lst;
+    *lst = new;      
 }
