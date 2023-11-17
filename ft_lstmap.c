b281c1d6d62519241d3dbd30329811a6f605ea8f
rda-cunh <ricard.mendes@gmail.com>
2023-11-17 18:56:15 +0000

    new

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 00:17:38 by rda-cunh          #+#    #+#             */
-/*   Updated: 2023/11/17 18:12:58 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2023/11/17 18:51:50 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void *))
 	new_node = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		new_node = ft_lstnew(f(lst -> content));
 		if (!new_node)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new_lst, new_node);
-		lst = lst->next;
+		lst = lst -> next;
 	}
 	return (new_lst);
 }
